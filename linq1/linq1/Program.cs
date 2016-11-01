c7162a6c003bc0f322112ac4fbf07db33c35a4a8
Koson Trachu <koson.trachu@gmail.com>
2016-11-02 02:30:10 +0700

    1

---
+++ linq1/linq1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "hello", "wonderful", "LINQ", "beautiful", "world" };
+        //Get only short words
+        var shortWords = from word in words
+                         where word.Length <= 5
+                         select word;
+
+        //Print each word out
+        foreach (var word in shortWords)
+        {
+            Console.WriteLine(word);
+        }
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
