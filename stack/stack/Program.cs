de266f35c44aba98c64d0a77577136a5be654119
Koson Trachu <koson.trachu@gmail.com>
2016-11-02 01:28:41 +0700

    1

---
+++ stack/stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+
+            st.Push('A');
+            st.Push('M');
+            st.Push('G');
+            st.Push('W');
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+
+            st.Push('V');
+            st.Push('H');
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values ");
+            st.Pop();
+            st.Pop();
+            st.Pop();
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
