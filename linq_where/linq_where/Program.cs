5e7615c715b4a8677495ca9dc882f4c2c8f7780c
Koson Trachu <koson.trachu@gmail.com>
2016-11-02 02:43:38 +0700

    1

---
+++ linq_where/linq_where/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "humpty", "dumpty", "set", "on", "a", "wall" };
+
+            IEnumerable<string> query = from word in words
+                                        where word.Length == 3
+                                        select word;
+            foreach (string str in query)
+                Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
